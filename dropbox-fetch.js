30c26047c64dcfd99ec426c65d975d37328eef7c
Janis Krasemann <krasemann.janis@gmail.com>
2016-06-12 18:06:44 +0200

    Implement `upload`

--- dropbox-fetch.js
+++ dropbox-fetch.js
@@ -1,10 +1,15 @@
+const fetch = require('node-fetch');
+
 /**
  * The authorization token with which calls to the API are made.
  * @type {String}
  */
-let token = '';
+let _token = '';
+
+const API_VERSION = '2/';
 
 const AUTHORIZE_ENDPOINT = 'https://www.dropbox.com/oauth2/authorize';
+const CONTENT_UPLOAD_ENDPOINT = 'https://content.dropboxapi.com/';
 
 const getAuthorizationUrl = (clientId) => {
   return AUTHORIZE_ENDPOINT + '?' +
@@ -32,27 +37,57 @@ const authorize = (clientId, redirectUri = '') => {
  * Set the token that is used for all Dropbox API calls to the given value.
  * @param  {string} t The new token value.
  */
-const setToken = (t) => {
-  if (typeof t !== 'string') {
-    throw new Error('invalid argument ' + t + ' (expected: string)');
+const setToken = (token) => {
+  if (typeof token !== 'string') {
+    throw new Error('invalid argument ' + token + ' (expected: string)');
   }
-  token = t;
+  _token = token;
 };
 
 /**
  * Upload the given file to the dropbox.
  * @param  {object} file an object describing the file, consisting of:
  *  - {string} name
- *  - {Buffer} content
- *  - {boolean} replace
+ *  - {string} content
+ *  - {string} mode
  * @param  {string} destinationPath the path in the dropbox where the file should
  * be uploaded to
+ * @param  {string?} token the OAuth 2 token that is used to access your app;
+ * can be omitted (in this case, the token that is set via ``setToken` is used)
  * @return {function} a promise that resolves when the upload is complete or
  * fails with an error message
  */
-const upload = ({ name, content, replace }, destinationPath) => {
-  return new Promise((resolve, reject) => {
-    reject('Not implemented yet');
+const upload = ({ name, content, mode }, destinationPath, token = _token) => {
+  if (typeof name !== 'string') {
+    throw new Error('invalid argument ' + name + ' (expected: string)');
+  }
+  if (typeof content !== 'string') {
+    throw new Error('invalid argument ' + content + ' (expected: string)');
+  }
+  if (typeof mode !== 'string') {
+    throw new Error('invalid argument ' + mode + ' (expected: string)');
+  }
+  if (typeof destinationPath !== 'string') {
+    throw new Error('invalid argument ' + destinationPath + ' (expected: string)');
+  }
+  if (typeof token !== 'string') {
+    throw new Error('invalid argument ' + token + ' (expected: string)');
+  }
+
+  const path = destinationPath + '/' + name;
+  return fetch(CONTENT_UPLOAD_ENDPOINT + API_VERSION + '/files/upload', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/octet-stream',
+      'Authorization': 'Bearer ' + token,
+      'Dropbox-API-Arg': JSON.stringify({
+        'path': path,
+        'mode': mode,
+        'autorename': true,
+        'mute': false
+      })
+    },
+    body: content
   });
 };
 
